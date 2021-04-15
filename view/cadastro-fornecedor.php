0235451aba7f6366994ba9b1e551560e68ddc98e
gfbarcellos <38571785+gfbarcellos@users.noreply.github.com>
2021-04-14 22:23:22 -0300

    inicio

---
+++ view/cadastro-fornecedor.php
@@ -0,0 +1,114 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastros</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link href="../styles/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container-fluid border">
+        <div class="row">
+            <div class="container-fluid border">
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover table-condensed">
+                        <thead>
+                            <tr>
+                                <th class="cadastro-fornecedor-tabela-col1">ID</th>
+                                <th class="cadastro-fornecedor-tabela-col2">Nome</th>
+                                <th class="cadastro-fornecedor-tabela-col3">Descrição</th>
+                                <th class="cadastro-fornecedor-tabela-col4">Telefone</th>
+                                <th class="cadastro-fornecedor-tabela-col5">Email</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    <div class="cadastro-fornecedor-container scrollable">
+                        <table class="table table-hover table-striped table-condensed cadastro-fornecedor-tabela">
+                            <tbody>
+                            <tr>
+                                <td class="cadastro-fornecedor-tabela-col1">2</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr>
+                            <tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr><tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr><tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr>
+                            <tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr>
+                            <tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr>
+                            <tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr>
+                            <tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr>
+                            <tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr>
+                            <tr>
+                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
+                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
+                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
+                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
+                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>

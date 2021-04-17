3699f9002ea30d520b03cf316600f40320b393c7
gfbarcellos <38571785+gfbarcellos@users.noreply.github.com>
2021-04-17 13:54:49 -0300

    forncedor

--- view/cadastro-fornecedor.php
+++ view/cadastro-fornecedor.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cadastros</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css">
     <link href="../styles/main.css" rel="stylesheet">
 
 </head>
@@ -18,96 +19,177 @@
 <body>
     <div class="container-fluid border">
         <div class="row">
+            <div class="col-md-6">
+                <div class="form-row row">
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="endereco">ID</label>
+                            <input type="text" class="form-control" id="id-fornecedor" placeholder="">
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="form-group">
+                            <label for="numero">Nome</label>
+                            <input type="numero" class="form-control" id="nome-fornecedor" placeholder="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+            <a class="btn btn-info cadastro-fornecedor-botao-pesquisar" href="path/to/settings" aria-label="Settings">
+                    <i class="fa fa-search"> Pesquisar</i>
+                </a>
+            </div>
             <div class="container-fluid border">
                 <div class="table-responsive">
                     <table class="table table-striped table-hover table-condensed">
                         <thead>
                             <tr>
-                                <th class="cadastro-fornecedor-tabela-col1">ID</th>
-                                <th class="cadastro-fornecedor-tabela-col2">Nome</th>
-                                <th class="cadastro-fornecedor-tabela-col3">Descrição</th>
-                                <th class="cadastro-fornecedor-tabela-col4">Telefone</th>
-                                <th class="cadastro-fornecedor-tabela-col5">Email</th>
+                                <th class="cadastro-fornecedor-tabela-col1">Editar</th>
+                                <th class="cadastro-fornecedor-tabela-col2">ID</th>
+                                <th class="cadastro-fornecedor-tabela-col3">Nome</th>
+                                <th class="cadastro-fornecedor-tabela-col4">Descrição</th>
+                                <th class="cadastro-fornecedor-tabela-col5">Telefone</th>
+                                <th class="cadastro-fornecedor-tabela-col6">Email</th>
+                                <th class="cadastro-fornecedor-tabela-col7">Eliminar</th>
                             </tr>
                         </thead>
                     </table>
                     <div class="cadastro-fornecedor-container scrollable">
-                        <table class="table table-hover table-striped table-condensed cadastro-fornecedor-tabela">
+                        <table class="table table-hover table-striped table-bordered table-condensed cadastro-fornecedor-tabela">
                             <tbody>
-                            <tr>
-                                <td class="cadastro-fornecedor-tabela-col1">2</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr>
-                            <tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr><tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr><tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr>
-                            <tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr>
-                            <tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr>
-                            <tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr>
-                            <tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr>
-                            <tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr>
-                            <tr>
-                                <td class="cadastro-fornecedor-tabela-col1">Mark</td>
-                                <td class="cadastro-fornecedor-tabela-col2">Otto</td>
-                                <td class="cadastro-fornecedor-tabela-col3">@mdo</td>
-                                <td class="cadastro-fornecedor-tabela-col4">878o</td>
-                                <td class="cadastro-fornecedor-tabela-col5">@mdo</td>
-                            </tr>
-                        </tbody>
+                                <tr>
+                                    <td class="cadastro-fornecedor-tabela-col1">
+                                        <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                                        </a>
+                                    </td>
+                                    <td class="cadastro-fornecedor-tabela-col2">1</td>
+                                    <td class="cadastro-fornecedor-tabela-col3">Fruteira</td>
+                                    <td class="cadastro-fornecedor-tabela-col4">Vende frutas</td>
+                                    <td class="cadastro-fornecedor-tabela-col5">12345678</td>
+                                    <td class="cadastro-fornecedor-tabela-col6">fruteira@gmail.com</td>
+                                    <td class="cadastro-fornecedor-tabela-col7">
+                                        <div class="container">
+                                            <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="cadastro-fornecedor-tabela-col1">
+                                        <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                                        </a>
+                                    </td>
+                                    <td class="cadastro-fornecedor-tabela-col2">1</td>
+                                    <td class="cadastro-fornecedor-tabela-col3">Fruteira</td>
+                                    <td class="cadastro-fornecedor-tabela-col4">Vende frutas</td>
+                                    <td class="cadastro-fornecedor-tabela-col5">12345678</td>
+                                    <td class="cadastro-fornecedor-tabela-col6">fruteira@gmail.com</td>
+                                    <td class="cadastro-fornecedor-tabela-col7">
+                                        <div class="container">
+                                            <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="cadastro-fornecedor-tabela-col1">
+                                        <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                                        </a>
+                                    </td>
+                                    <td class="cadastro-fornecedor-tabela-col2">1</td>
+                                    <td class="cadastro-fornecedor-tabela-col3">Fruteira</td>
+                                    <td class="cadastro-fornecedor-tabela-col4">Vende frutas</td>
+                                    <td class="cadastro-fornecedor-tabela-col5">12345678</td>
+                                    <td class="cadastro-fornecedor-tabela-col6">fruteira@gmail.com</td>
+                                    <td class="cadastro-fornecedor-tabela-col7">
+                                        <div class="container">
+                                            <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="cadastro-fornecedor-tabela-col1">
+                                        <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                                        </a>
+                                    </td>
+                                    <td class="cadastro-fornecedor-tabela-col2">1</td>
+                                    <td class="cadastro-fornecedor-tabela-col3">Fruteira</td>
+                                    <td class="cadastro-fornecedor-tabela-col4">Vende frutas</td>
+                                    <td class="cadastro-fornecedor-tabela-col5">12345678</td>
+                                    <td class="cadastro-fornecedor-tabela-col6">fruteira@gmail.com</td>
+                                    <td class="cadastro-fornecedor-tabela-col7">
+                                        <div class="container">
+                                            <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="cadastro-fornecedor-tabela-col1">
+                                        <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                                        </a>
+                                    </td>
+                                    <td class="cadastro-fornecedor-tabela-col2">1</td>
+                                    <td class="cadastro-fornecedor-tabela-col3">Fruteira</td>
+                                    <td class="cadastro-fornecedor-tabela-col4">Vende frutas</td>
+                                    <td class="cadastro-fornecedor-tabela-col5">12345678</td>
+                                    <td class="cadastro-fornecedor-tabela-col6">fruteira@gmail.com</td>
+                                    <td class="cadastro-fornecedor-tabela-col7">
+                                        <div class="container">
+                                            <a class="btn btn-default" href="path/to/settings" aria-label="Settings">
+                                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="container-fluid border">
+        <form class="cadastro-fornecedor-form">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" id="nome">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="telefone">Telefone</label>
+                    <input type="text" class="form-control" id="telefone" placeholder="(##) #####-####">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="descricao">Descrição</label>
+                    <input type="text" class="form-control" id="descricao">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="email">Email</label>
+                    <input type="text" type="email" class="form-control" id="email" placeholder="exemplo@exemplo.com">
+                </div>
+            </div>
+            <a class="btn btn-success" href="path/to/settings" aria-label="Settings">
+                <i class="fa fa-save" aria-hidden="true"> Salvar</i>
+            </a>
+            <a class="btn btn-light" href="path/to/settings" aria-label="Settings">
+                <i class="fa fa-eraser"> Limpar</i>
+            </a>
+        </form>
     </div>
 </body>
 
